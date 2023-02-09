25dfa5d0531402491b5ce4786c492ea2e3c36dc0
chris <christo1771@gmail.com>
2023-02-09 12:40:15 -0500

    fix(CI): add semantic-release for automated release

---
+++ scripts/npm_publish.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+VERSION="$1"
+
+echo "============================"
+echo "= Publish version $VERSION ="
+echo "============================"
+
+echo "Set convert wasm package version"
+cargo bump "$VERSION" -k convert
+
+echo "Build npm package"
+just build-convert-release
+
+echo "Publish npm package"
+cd wasm-package || exit;
+npm publish --access=public
